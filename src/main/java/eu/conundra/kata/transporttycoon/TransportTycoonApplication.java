6b2e543443d4f92a37ce73deb83805c07f0e003f
Karel Vervaeke <karel.vervaeke@conundra.eu>
2023-03-17 13:29:59 +0100

    Seed project

---
+++ src/main/java/eu/conundra/kata/transporttycoon/TransportTycoonApplication.java
@@ -0,0 +1,13 @@
+package eu.conundra.kata.transporttycoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportTycoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportTycoonApplication.class, args);
+	}
+
+}
