1aac859a24a22965899b8f39df96df424e9b337d
Michel Grootjans <michel.grootjans@conundra.eu>
2023-03-18 09:27:46 +0100

    first ugly implementation

---
+++ src/main/java/eu/conundra/kata/transporttycoon/Transporter.java
@@ -0,0 +1,77 @@
+package eu.conundra.kata.transporttycoon;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Transporter {
+    private final Queue<String> factoryPackages;
+    private final List<String> bPackages = new ArrayList<>();
+    private int position1 = 0;
+    private String payload1 = "";
+    private int position2 = 0;
+    private String payload2 = "";
+
+    public Transporter(List<String> factoryPackages) {
+        this.factoryPackages = new LinkedList<>(factoryPackages);
+    }
+
+    public int solve() {
+        do {
+            dropPackage();
+            pickUpPackage();
+            move();
+            if(!done()) iterations++;
+        } while (!done());
+        return iterations;
+    }
+
+    private void dropPackage() {
+        if (position1 == 5) {
+            bPackages.add(payload1);
+            payload1 = "";
+        }
+        if (position2 == 5) {
+            bPackages.add(payload2);
+            payload2 = "";
+        }
+    }
+
+    private void pickUpPackage() {
+        if (factoryPackages.isEmpty()) {
+            return;
+        }
+        if (position1 == 0) {
+            payload1 = factoryPackages.remove();
+        }
+        if (factoryPackages.isEmpty()) {
+            return;
+        }
+        if (position2 == 0) {
+            payload2 = factoryPackages.remove();
+        }
+    }
+
+    private void move() {
+        if (payload1.equals("")) {
+            position1--;
+        } else {
+            position1++;
+        }
+        if (payload2.equals("")) {
+            position2--;
+        } else {
+            position2++;
+        }
+    }
+
+    private int iterations = 0;
+
+    private boolean done() {
+        if (iterations > 100) {
+            return true;
+        }
+        return payload1.equals("") && payload2.equals("") && factoryPackages.isEmpty();
+    }
+}
---
+++ src/test/java/eu/conundra/kata/transporttycoon/TransportTest.java
@@ -0,0 +1,33 @@
+package eu.conundra.kata.transporttycoon;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class TransportTest {
+    @Test
+    void testB() {
+        List<String> packages = List.of("B");
+
+        Transporter transporter = new Transporter(packages);
+        assertThat(transporter.solve()).isEqualTo(5);
+    }
+
+    @Test
+    void testBB() {
+        List<String> packages = List.of("B", "B");
+
+        Transporter transporter = new Transporter(packages);
+        assertThat(transporter.solve()).isEqualTo(5);
+    }
+
+    @Test
+    void testBBB() {
+        List<String> packages = List.of("B", "B", "B");
+
+        Transporter transporter = new Transporter(packages);
+        assertThat(transporter.solve()).isEqualTo(15);
+    }
+}
